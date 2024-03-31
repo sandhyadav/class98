9d41cec8d3127d8f9601cb9f6e78fd0527eef70c
sandhyadav <66108338+sandhyadav@users.noreply.github.com>
2024-03-31 12:31:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+  
+var recognition = new SpeechRecognition();
+
+function start()
+{
+    document.getElementById("textbox").innerHTML = ""; 
+    recognition.start();
+} 
+ 
+recognition.onresult = function(event) {
+
+ console.log(event); 
+
+var Content = event.results[0][0].transcript;
+
+    document.getElementById("textbox").innerHTML = Content;
+    console.log(Content);
+      if(Content =="take my selfie")
+      {
+        console.log("taking selfie --- ");
+        speak();
+      }
+}
+
+
+
+ 
+
+function save()
+{
+  link = document.getElementById("link");
+  image = document.getElementById("selfie_image").src ;
+  link.href = image;
+  link.click();
+}
